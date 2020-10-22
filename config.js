054dbb0320f016c33aa31a112f8c64dd957dda5f
ragon3 <66675072+ragon3@users.noreply.github.com>
2020-10-22 12:30:38 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   prefix: "-",
   devs: ["500021254317670400"], ///تعديل اساسي ايدي حسابك
-  Token: ["NzQ4MzAwMjUzMzgzNDkxNzI0.X0ba6w.2eH732OxS2U5esCrVRn3gxizLBg"],
+  Token: ["NzQ4MzAwMjUzMzgzNDkxNzI0.X0ba6w.blq9HUCp0UtK4ctkYtAlqg8AcFg"],
 pass:1
 };
