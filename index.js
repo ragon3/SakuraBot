78df11749e6d0810b16ce7ea47e80e4ca75e2f18
ragon3 <66675072+ragon3@users.noreply.github.com>
2020-10-26 17:24:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -521,23 +521,40 @@ client.on("message", message => {
 
 ///-===-===-===-=== ban , unban Message ===-===-===-===-///
 
-   client.on("message", message => {
-  if (message.content.startsWith(prefix + "ban")) {
-   let embed = new Discord.RichEmbed()
+
+client.on('message', message => {
+  if (message.author.x5bz) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "ban") {
+       let embed = new Discord.RichEmbed()
             .setColor('#288bb8')
-      .setThumbnail(message.guild.iconURL)
-            .setAuthor(client.user.username,client.user.avatarURL)
-            .setThumbnail(client.user.avatarURL)
-            .setColor('#288bb8')
-            .setTitle('** Ban Commands:  **')
-            .addField('** ** **Examples: :**' , [`» $ban ` + '[ @User ] [ time ends with m,h,d,mo,y ] [ reason ] ' ], true)
+    if (!message.channel.guild) return;
+         
+  if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.reply("**You Don't Have ` BAN_MEMBERS ` Permission**");
+  if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.reply("**I Don't Have ` BAN_MEMBERS ` Permission**");
+  let user = message.mentions.users.first();
+  let reason = message.content.split(" ").slice(2).join(" ");
+  /*let b5bzlog = client.channels.find("name", "5bz-log");
+  if(!b5bzlog) return message.reply("I've detected that this server doesn't have a 5bz-log text channel.");*/
+  if (message.mentions.users.size < 1) return message("** ** **Examples: :**' , [`» $ban ` + '[ @User ] [ time ends with m,h,d,mo,y ] [ reason ]");
+  if(!reason) return;
+  if (!message.guild.member(user)
+  .bannable) return message.reply("**This person has a grade higher than his bot rank**");
+
+  message.guild.member(user).ban(7, user);
+  message.channel.send(`**:white_check_mark: ${user} has been banned :airplane: **`)
+user.send(`You Are Has Been Banned Permanently In ${message.guild.name} reason: ${reason}`)
+          message.channel.send({embed:embed});
+
+  }})
 
 
-      message.channel.send({embed:embed});
-
-    
-}
-});
 
 
   client.on("message", message => {
